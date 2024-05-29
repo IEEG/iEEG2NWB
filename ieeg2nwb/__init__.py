e6b3c3f39cd5c221fcaa254f310a8a008b4b23c8
NoahMarkowitz <noahb.markowitz@gmail.com>
2024-05-29 14:00:03 -0400

    initial population

---
+++ ieeg2nwb/__init__.py
@@ -0,0 +1,2 @@
+from .ieeg2nwb import iEEG2NWB
+from .utils import load_nwb_settings, inspectNwb
---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='iEEG2NWB',
+    description='Python scripts for converting ieeg data in edf and TDT formats to NWB',
+    version='0.0.1',
+    author='Noah Markowitz, Stephan Bickel',
+    #include_package_data=True,
+    packages=['ieeg2nwb'],
+    package_dir={'':'Python'},
+    entry_points={
+        'console_scripts': [
+            'ieeg2nwb=ieeg2nwb.ieeg2nwb:cmnd_line_parser'
+            ]
+    }
+)
